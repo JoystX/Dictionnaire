a20d715fc8a4e314dac794a6ecf78668f5f26d65
aryus96 <aryus96@hotmail.fr>
2015-10-09 22:58:28 +0200

    Ajout du sujet et du main

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int choix;
+    printf("Bienvenu sur Dictionnaire V2\n");
+
+    printf("Choisissez votre option :\n");
+
+
+    scanf(&choix,"%d");
+
+    return 0;
+}
